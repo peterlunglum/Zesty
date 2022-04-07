6466d25553dee7a21b9bbeed080741fc3908bdd2
peterlunglum <peter.lung.lum@gmail.com>
2022-04-07 00:30:32 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import "./styles.css";
+import React, { useState } from "react";
+import cabbage from "./assets/image1.jpeg";
+import mango from "./assets/image2.jpeg";
+import fig from "./assets/image3.jpeg";
+import gaze from "./assets/image4.jpeg";
+import peach from "./assets/image5.jpeg";
+import avocado from "./assets/image6.jpeg";
+
+const images = [cabbage, mango, fig, gaze, peach, avocado];
+const Loading = ({ calculatedWidth }) => (
+  <aside>
+    <div className="loading-bar">
+      <label htmlFor="images-loaded">Loading all your favorite images...</label>
+      <progress id="images-loaded" max="100" value={calculatedWidth}></progress>
+    </div>
+  </aside>
+);
+
+const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [numLoaded, setNumLoaded] = useState(0);
+  const handleClick = () => {
+    const length = images.length - 1;
+
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0;
+    });
+  };
+
+  const handleImageLoad = () => {
+    setNumLoaded((numLoaded) => numLoaded + 1);
+  };
+
+  return (
+    <section>
+      <header>
+        <h1>Zesty</h1>
+        <h2>
+          A photography project <br /> by Ella Fieldling
+        </h2>
+        {/* <button onClick={handleClick}>click here</button> */}
+      </header>
+      <figure>
+        {numLoaded < images.length && (
+          <Loading calculatedWidth={(numLoaded / images.length) * 100} />
+        )}
+
+        <figcaption>
+          {currentImage + 1} / {images.length}
+        </figcaption>
+        {images.map((imageURL, index) => (
+          <img
+            alt=""
+            key={imageURL}
+            src={imageURL}
+            onClick={handleClick}
+            onLoad={handleImageLoad}
+            //style={{opacity: currentImage === index ? 1 : 0}}
+            className={currentImage === index ? "display" : "hide"}
+          />
+        ))}
+      </figure>
+    </section>
+  );
+};
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// };
+
+export default App;
